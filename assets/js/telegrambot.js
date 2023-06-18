1e45bb2dfb1a1553ce28d1144d6bd37cdc1bd1a7
Muhammad Herdy Iskandar <66510723+dyzulk@users.noreply.github.com>
2023-06-18 22:18:20 +0700

    Update telegrambot.js

--- assets/js/telegrambot.js
+++ assets/js/telegrambot.js
@@ -1,13 +1,14 @@
 function kirimPesan() {
 
-    var nama = document.getElementById('nama');
+    var nama = document.getElementById('name');
     var email = document.getElementById('email');
-    var pesan = document.getElementById('pesan');
+    var email = document.getElementById('subject');
+    var pesan = document.getElementById('message');
 
-    var gabungan = 'Nama%3A%0A' + nama.value + '%0AEmail%3A%0A' + email.value + '%0APesan%3A%0A' + pesan.value;
+    var gabungan = 'Nama%3A%0A' + name.value + '%0AEmail%3A%0A' + email.value + '%0ASubject%3A%0A' + subject.value + '%0APesan%3A%0A' + message.value;
 
-    var token = 'TOKEN_BOT_KAMU'; // Ganti dengan token bot yang kamu buat
-    var grup = 'CHAT_ID_GRUP_KAMU'; // Ganti dengan chat id dari bot yang kamu buat
+    var token = '6260715907:AAHYHXLrAwSjnUecHxLi0AC01k1FM0xbr8M'; // Ganti dengan token bot yang kamu buat
+    var grup = '-836942032'; // Ganti dengan chat id dari bot yang kamu buat
 
     $.ajax({
         url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
