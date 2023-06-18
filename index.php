e41151ec1155f1dcd33f3a70d9b37fc21a7a8fe0
Muhammad Herdy Iskandar <66510723+dyzulk@users.noreply.github.com>
2023-06-18 21:20:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/assets/php/"; include($IPATH."header.php")?>
+        <link href="favicon.png" rel="icon" sizes=”32×32″ type="image/png">
+	<meta charset="UTF-8">
+	<meta charset="UTF-8">
+	<meta name="description" content="Muhammad Herdy Iskandar He Is A 18 year old Indonesian student living in Cirebon, Indonesian. You might have seen me on Facebook or Instagram. Now here you are. Yes, I write, too. Because there are so many things I cannot say out loud, yet I always find a way to put it into words.">
+	<meta name="keywords" content="Muhammad Herdy Iskandar, HTML,CSS,React JS,JavaScript">
+	<meta name="author" content="Muhammad Herdy Iskandar">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="google-site-verification" content="C7cNjxkyxXcQHExRsdhIGjZ658BcK9ftuIpJK8NYRGc" />
+	<link rel="stylesheet" href="assets/css/styles.css">
+	<!-- =====BOX ICONS===== -->
+	<!-- =====BOX ICONS===== -->
+	<link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+	
+	<title>Muhammad Herdy Iskandar</title>
     </head>
     <body>
         <main class="l-main">
@@ -160,13 +173,21 @@
 				</div>
             </section>
         </main>
-
-        <?php include($IPATH."footer.php")?>
-
+	
         <!--===== SCROLL REVEAL =====-->
         <script src="https://unpkg.com/scrollreveal"></script>
 
         <!--===== MAIN JS =====-->
         <script src="assets/js/main.js"></script>
     </body>
+	        <!--===== FOOTER =====-->
+        <footer class="footer">
+            <p class="footer__title">myPortfolio</p>
+            <div class="footer__social">
+                <a href="https://facebook.com/dyzulk" class="footer__icon"><i class='bx bxl-facebook' ></i></a>
+                <a href="https://instagram.com/dyzulk" class="footer__icon"><i class='bx bxl-instagram' ></i></a>
+                <a href="https://twitter.com/dyzulk_" class="footer__icon"><i class='bx bxl-twitter' ></i></a>
+            </div>
+            <p class="footer__copy">Copyright &#169; 2022 - <script>document.write(new Date().getFullYear())</script>  <a href="/" style="color: #fff;">Herdy Iskandar</a>. All rigths reserved</p>
+        </footer>
 </html>
