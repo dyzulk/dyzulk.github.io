34b71577c253ec4d04cd6f993830e2169adee2f1
Muhammad Herdy Iskandar <66510723+dyzulk@users.noreply.github.com>
2023-06-18 22:35:01 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,22 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+/*===== TELEGRAM BOT =====*/
+function kirimPesan() {
+
+    var nama = document.getElementById('name');
+    var email = document.getElementById('email');
+    var email = document.getElementById('subject');
+    var pesan = document.getElementById('message');
+
+    var gabungan = 'Nama%3A%0A' + name.value + '%0AEmail%3A%0A' + email.value + '%0ASubject%3A%0A' + subject.value + '%0APesan%3A%0A' + message.value;
+
+    var token = '6260715907:AAHYHXLrAwSjnUecHxLi0AC01k1FM0xbr8M'; // Ganti dengan token bot yang kamu buat
+    var grup = '-836942032'; // Ganti dengan chat id dari bot yang kamu buat
+
+    $.ajax({
+        url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
+        method: `POST`,
+    })
+}
